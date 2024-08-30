83248d5a003e14198534261a9ace55721821df99
Julian David <juli.rivera.quintero@gmail.com>
2024-08-29 21:22:35 -0500

    feat(DOM): Manipulacion de un elemento del documento

---
+++ Manipulacion-No-Reactiva/Manipular elemento/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+//Manipular un elemento que viene del documento
+const titulo = document.getElementById('titulo');
+
+//Le inserto o asigno a la variable
+titulo.innerHTML = 'Esto esta manipulado con el DOM';
