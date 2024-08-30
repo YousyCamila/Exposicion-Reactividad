5ae3812a92c2f9b5196d6ee7b4d89f3f19740cfa
Julian David <juli.rivera.quintero@gmail.com>
2024-08-29 21:11:40 -0500

    feat(DOM):Creacion de un elemento...

---
+++ Manipulacion-No-Reactiva/Crear-elemento/script.js
@@ -0,0 +1,13 @@
+// Crear el elemento <p>
+const parrafo = document.createElement('p');
+
+// Asignar contenido al <p>
+parrafo.innerHTML = 'Esto es un parrafo';
+
+//Agregar el <p> al cuerpo del documento
+document.body.appendChild(parrafo);
+
+//Agregar estilos
+document.body.style.color = 'blue';
+
+
